b053a60cf1a2448de09dc3c063be98ec0d3d01b0
ea599 <ea599@kent.ac.uk>
2024-10-09 11:14:28 +0100

    Workshop 3.1 ea599

--- partOne.py
+++ partOne.py
@@ -1,1 +1,11 @@
+def main():
+    age = int(input("What is your age? "))
+    print(adultCheck(age))
 
+def adultCheck(age):
+    if age >= 18:
+        return "adult"
+    else:
+        return "child"
+    
+main()
